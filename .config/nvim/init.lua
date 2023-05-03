107ea0c696e82d5796850a1e82e4485875973942
Vylssius <vylssius@hotmail.com>
2023-05-02 21:43:57 -0500

    sietn

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- local pywal = require("pywal")
--- pywal.setup()
+local pywal = require("pywal")
+pywal.setup()
 require("leap").add_default_mappings()
